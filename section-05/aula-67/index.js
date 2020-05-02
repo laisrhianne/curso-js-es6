0a4ae29d6f7c5b6316ad392c0c2a5ae57815749f
laisrhianne <lays.rhianne@gmail.com>
2020-05-02 13:12:46 -0300

    aulas 68 e 69

--- section-05/aula-67/index.js
+++ section-05/aula-67/index.js
@@ -2,12 +2,12 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-const total = numeros.reduce(function (acumulador, valor, indice, array){
+const total = numeros.reduce(function (acumulador, valor, indice, array) {
     acumulador += valor;
     return acumulador;
 }, 0);
 
-const somaPares = numeros.reduce(function (acumulador, valor){
+const somaPares = numeros.reduce(function (acumulador, valor) {
     if (valor % 2 === 0) acumulador += valor;
     return acumulador;
 })
@@ -16,12 +16,12 @@ const somaPares = numeros.reduce(function (acumulador, valor){
 console.log(somaPares);
 
 const pessoas = [
-    {nome: 'Laís', idade: 23},
-    {nome: 'Bruna', idade: 25},
-    {nome: 'Manuella', idade: 24},
-    {nome: 'Josué', idade: 27},
-    {nome: 'Jhon', idade: 25},
-    {nome: 'Abilash', idade: 18}
+    { nome: 'Laís', idade: 23 },
+    { nome: 'Bruna', idade: 25 },
+    { nome: 'Manuella', idade: 24 },
+    { nome: 'Josué', idade: 27 },
+    { nome: 'Jhon', idade: 25 },
+    { nome: 'Abilash', idade: 18 }
 ];
 
 const maisVelha = pessoas.reduce(function (n, obj) {
---
+++ section-05/aula-69/index.js
@@ -0,0 +1,7 @@
+// For each
+
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+a1.forEach(function (valor, indice, array){
+    console.log(valor, indice);
+});
