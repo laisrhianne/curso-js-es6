90e4176d1d5c4fe3608665c65c61439157cd993e
laisrhianne <lays.rhianne@gmail.com>
2020-06-15 20:58:54 -0300

    aula 167

--- section-13-api-rest/src/controllers/HomeController.js
+++ section-13-api-rest/src/controllers/HomeController.js
@@ -2,15 +2,7 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Marta',
-      sobrenome: 'Lisboa',
-      email: 'marta@gmail.com',
-      idade: 48,
-      peso: 72.3,
-      altura: 1.58,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
