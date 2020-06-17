c988c0131e84ac20991df9ba9ec57c3b3cf78042
Laís Rhianne <lays.rhianne@gmail.com>
2020-06-17 20:04:40 -0300

    aula 171

--- section-13-api-rest/src/routes/fotoRoutes.js
+++ section-13-api-rest/src/routes/fotoRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import loginRequired from '../middlewares/loginRequired';
 
 import fotoController from '../controllers/FotoController';
 
 const router = new Router();
 
-router.get('/', fotoController.store);
+router.get('/', loginRequired, fotoController.store);
 
 export default router;
